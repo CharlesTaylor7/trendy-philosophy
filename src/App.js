154fddbf15ebfe384a110ecfc4958a5e6ebc9a5c
Charles Taylor <charlest@paragontruss.com>
2019-07-27 19:25:44 -0400

    Only respond to query after 400 ms

--- src/App.js
+++ src/App.js
@@ -1,15 +1,22 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect, useMemo, useCallback } from 'react';
 import './App.css';
 import { Graph } from './components/Graph';
 import { QueryInput } from './components/QueryInput';
 import { record$ } from './dataSources/philPapers';
+import { Subject } from 'rxjs';
+import { debounceTime } from 'rxjs/operators';
 
 export const App = () => {
   const [ query, setQuery ] = useState('');
+
+  const input$ = useMemo(() => new Subject().pipe(debounceTime(400)));
+  const onInput = input => input$.next(input);
+  useEffect(() => input$.subscribe(setQuery), []);
+
   return (
     <div className="App">
       <header className="App-header">
-        <QueryInput onInput={setQuery}/>
+        <QueryInput onInput={onInput}/>
         <Graph
           query={query}
           record$={record$}
