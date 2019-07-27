934900dbc9734112efbf878f9cdd3fa0fc6a714a
Charles Taylor <charlest@paragontruss.com>
2019-07-27 13:35:17 -0400

    search box

--- src/App.js
+++ src/App.js
@@ -1,15 +1,27 @@
-import React, { useEffect } from 'react';
+import React, { useState } from 'react';
 import './App.css';
 import { Graph } from './components/Graph';
 // import { record$ } from './dataSources/philosophyPaperboy';
 import { record$ } from './dataSources/philPapers';
 
 export const App = () => {
-  const query = 'family';
+  const [ query, setQuery ] = useState('');
+
+  const onChange = event => setQuery(event.target.value);
+
   return (
     <div className="App">
       <header className="App-header">
-        <p>Trending: "{query}"</p>
+        {/* <p>Trending: "{query}"</p> */}
+        <label>
+            Trend for:
+            <input
+              type="text"
+              value={query}
+              onChange={onChange}
+              style={{margin:'10px'}}
+            />
+          </label>
         <Graph
           query={query}
           record$={record$}
